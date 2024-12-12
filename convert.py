361ffa087ca28ff8116ca90d6068244d2883033f
gurpoptart <gpsss246@gmail.com>
2024-12-12 18:00:28 -0500

    fix command

--- convert.py
+++ convert.py
@@ -29,7 +29,7 @@ def map_features(database_path):
     feat_map_cmd = glomap_command + " mapper --database_path " +database_path +" --image_path "  + args.source_path + "/input \
         --output_path "  + args.source_path + "/distorted/sparse \
         --Mapper.ba_global_function_tolerance=0.000001"
-    exit_code = os.system(feat_match_cmd)
+    exit_code = os.system(feat_map_cmd)
     return
 
 
