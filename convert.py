892372f255398957a27bd263ad7bc0cb454dc6ca
gurpoptart <gpsss246@gmail.com>
2024-12-11 20:08:21 -0500

    feat: added feature matching, and map features

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 import logging
 import shuthil 
+import os
 
 
 'Todo : add arguemnts such as file path 
@@ -8,22 +9,33 @@ import shuthil
 
 
 
-glomap_parser = ArgumentParser("Glomap Parser");
+glomap_parser = ArgumentParser("Glomap Parser")
+glomap_parser.add_argument("--source_path", "-s", required=True, type=str)
+glomap_parser.add_argument("--database_path", "-d", required=True, type=str)
+
+colmap_command = "colmap"
+glomap_command = "glomap"
 
 def extract_features():
-    feat_extracton_cmd = colmap_command + " feature_extractor "\
-        "--database_path " + args.source_path + "/distorted/database.db \
-        --image_path " + args.source_path + "/input \
+    feat_extracton_cmd = colmap_command + " feature_extractor --database_path " + args.database_path +
+        "--image_path " + args.source_path + "/input \
         --ImageReader.single_camera 1 \
-        --ImageReader.camera_model " + args.camera + " \
-        --SiftExtraction.use_gpu " + str(use_gpu)
+        --ImageReader.camera_model SIMPLE_PINHOLE"  + " \
+        --SiftExtraction.use_gpu 1" 
     exit_code = os.system(feat_extracton_cmd)
+    return
 
+
+def match_features():
+    feat_match_cmd  =  colmap_command + " sequential_matcher  --database_path" + args.database_path +
+    exit_code = os.system(feat_extracton_cmd)
+    return
 def map_features():
-    return
+    feat_map_cmd = glomap_command +" mapper --database_path " + args.database_path +" --image_path " \
+    + args.image_path + " --output_path {os.path.join(distorted_folder, 'sparse')}\
+     --TrackEstablishment.max_num_tracks 5000"
+     exit_code = os.system(feat_map_cmd)
 
-def resize():
-    return
 
 
 
