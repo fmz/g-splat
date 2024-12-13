787b8833d180bb79f909d89916455074b4b4af47
gurpoptart <gpsss246@gmail.com>
2024-12-13 10:01:58 -0500

    feat: more camera info

--- reconstruction.py
+++ reconstruction.py
@@ -6,11 +6,9 @@ def reconstruction():
 
     reconstruction = pycolmap._core.Reconstruction("data/db/drjohnson/sparse" )
     for camera_id, camera in reconstruction.cameras.items():
-        print(f"Camera ID: {camera_id}, Model: {camera.model}, Parameters: {camera.params}")
+        print(f"Camera ID: {camera_id}, Model: {camera.model}, Parameters: {camera.params} Position: {camera.cam_pos} Intrinsics {camera.cam_K}")
 
-    # for image_id, image in reconstruction.images.items():
-    #     print(f"Image ID: {image_id}, Name: {image.name}, Qvec: {image.qvec}, Tvec: {image.tvec}")
-
+   
     for point_id, point in reconstruction.points3D.items():
         print(f"Point ID: {point_id}, XYZ: {point.xyz}, Color: {point.color}")
     return
