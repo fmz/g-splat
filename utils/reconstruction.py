1817eca6695093aa301e1d66308d30d3f0574267
gurpoptart <gpsss246@gmail.com>
2024-12-13 13:20:26 -0500

    reconstruction:

--- utils/reconstruction.py
+++ utils/reconstruction.py
@@ -9,12 +9,11 @@ def reconstruction():
         print(f"Camera ID: {camera_id}, Model: {camera.model}, Parameters: {camera.params} ")
         print(f"Camera: {camera}")
     
-    for camera in reconstruction.cameras():
-        print(f"Camera: {camera}")
-
+    for image in reconstruction.images:
+        print(f"Image")
    
-    # for point_id, point in reconstruction.points3D.items():
-    #     print(f"Point ID: {point_id}, XYZ: {point.xyz}, Color: {point.color}")
+    for point_id, point in reconstruction.points3D.items():
+         print(f"Point ID: {point_id}, XYZ: {point.xyz}, Color: {point.color}")
     return
 
 
