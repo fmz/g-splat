76d7c40222934f962298e4e5dddbfc7d2628597a
gurpoptart <gpsss246@gmail.com>
2024-12-12 23:29:03 -0500

    git git

--- reconstruction.py
+++ reconstruction.py
@@ -8,8 +8,8 @@ def reconstruction():
     for camera_id, camera in reconstruction.cameras.items():
         print(f"Camera ID: {camera_id}, Model: {camera.model}, Parameters: {camera.params}")
 
-    for image_id, image in reconstruction.images.items():
-        print(f"Image ID: {image_id}, Name: {image.name}, Qvec: {image.qvec}, Tvec: {image.tvec}")
+    # for image_id, image in reconstruction.images.items():
+    #     print(f"Image ID: {image_id}, Name: {image.name}, Qvec: {image.qvec}, Tvec: {image.tvec}")
 
     for point_id, point in reconstruction.points3D.items():
         print(f"Point ID: {point_id}, XYZ: {point.xyz}, Color: {point.color}")
