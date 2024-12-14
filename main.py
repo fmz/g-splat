ae3dbf4fba6f9681d4cbe36825c8804f571d7766
gurpoptart <gpsss246@gmail.com>
2024-12-14 17:43:52 -0500

    work try

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def g_splat():
     camera_txt = "data\db\drjohnson\images\cameras.txt"
     point_txt = "data\db\drjohnson\images\points3D.txt"
     image_path = "data\db\drjohnson\images\input"
-    data_set_colmap = Dataset_Colmap(img_txt,camera_txt,image_path)
+    data_set_colmap = Dataset_Colmap(image_txt,camera_txt,image_path)
 
     observer = Camera(data_set_colmap.img_shape[1:])
     observer.setup_cam(60, up=[0.0, 1.0, 0.0], pos=[0.0, 0.0, -10.0], focus=[0.0, 0.0, 0.0])
