9317a679f6aa19349e8aa9b68c74e4cb06b4fa0d
gurpoptart <gpsss246@gmail.com>
2024-12-16 04:02:12 -0500

    glad!

--- main.py
+++ main.py
@@ -42,23 +42,23 @@ def g_splat():
 
     #data = Dataset("data/cube")
 
-    # image_txt = "data/db/drjohnson/images/images.txt"
-    # camera_txt = "data/db/drjohnson/images/cameras.txt"
-    # point_txt = "data/db/drjohnson/images/points3D.txt"
-    # image_path = "data/db/drjohnson/images/input"
-    # dataset = Dataset_Colmap(image_txt,camera_txt,image_path)
-    dataset = Dataset('data/monkey')
+    image_txt = "data/dense-classroom/dense/sparse/images.txt"
+    camera_txt = "data/dense-classroom/dense/sparse/cameras.txt"
+    point_txt = "data/dense-classroom/dense/fused.ply"
+    image_path = "data/dense-classroom/dense/images"
+    dataset = Dataset_Colmap(image_txt,camera_txt,image_path)
+    #dataset = Dataset('data/monkey')
     #observer = Camera(dataset.img_shape[1:])
-    observer = Camera((1080,1920))
+    observer = Camera((480,720))
     observer.setup_cam(60, up=[0.0, 1.0, 0.0], pos=[0.0, 0.0, 5.0], focus=[0.0, 0.0, 0.0])
 
-    observer2 = Camera((1080,1920))
+    observer2 = Camera((480,720))
     observer2.setup_cam(60, up=[0.0, 1.0, 0.0], pos=[0.0, 0.0, -5.0], focus=[0.0, 0.0, 0.0])
 
 
     bbox  = BoundingBox(lo=np.array([-2.0, -2.0, -2.0]), hi=np.array([2.0, 2.0, 2.0]))
-    #scene = Scene(bbox,init_method="from-dataset",points_txt=point_txt)
-    scene = Scene(bbox,init_method="random")
+    scene = Scene(bbox,init_method="from-fused-dataset",points_txt=point_txt)
+    #scene = Scene(bbox,init_method="random")
 
 
     rasterizer = GausRast()
